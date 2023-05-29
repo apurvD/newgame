05149453bfd4739a9668c486a1dcdb5638565df2
Apurv Deshpande <86904145+apurvD@users.noreply.github.com>
2023-05-29 22:27:21 +0530

    Update

--- bfs.py
+++ bfs.py
@@ -7,23 +7,6 @@ graph = {
     '6': [],
     '7': []
 }
-# visited=[]
-# queue=[]
-
-# def bfs(visited,graph,node):
-#     visited.append(node)
-#     queue.append(node)
-#     while queue:
-#         m=queue.pop(0)
-#         print(m,end=" ")
-#         for neighbor in graph:
-#             if neighbor not in visited:
-#                 visited.append(neighbor)
-#                 queue.append(neighbor)
-
-# print("Following is the BFS result:\n")
-
-# bfs(visited,graph,'2')
 
 
 def bfs_recursive(graph, queue, visited):
@@ -39,17 +22,6 @@ def bfs_recursive(graph, queue, visited):
             queue.append(neighbor)
 
     bfs_recursive(graph, queue, visited)
-
-# Example usage
-graph = {
-    '2': ['1', '3'],
-    '1': ['4', '5'],
-    '3': ['6', '7'],
-    '4': [],
-    '5': [],
-    '6': [],
-    '7': []
-}
 
 starting_node = '2'
 visited_nodes = set([starting_node])
